fa9ac4f1cc2941b30b9bee4304c9e59a29df4d47
Yannick Marcon <yannick.marcon@obiba.org>
2020-12-16 11:37:09 +0100

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'amber-electron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
